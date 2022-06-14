53012f41616f7357c4052cff6e1d3d4907913f6d
sachin9837 <psachin84453@gmail.com>
2022-06-14 08:23:30 +0530

    i am there

---
+++ src/main/java/mind_new/hello/App.java
@@ -0,0 +1,13 @@
+package mind_new.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
